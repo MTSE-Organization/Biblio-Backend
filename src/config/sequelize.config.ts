75ffa424cefec95b0fb7a4e100f2b97f2673dcdc
huypham <huyphuocbinh204@gmail.com>
2025-08-28 20:54:59 +0700

    refactor config

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -6,21 +6,20 @@ import {
   Permission,
 } from '@/models';
 import { ConfigService } from '@nestjs/config';
-import { SequelizeModuleOptions } from '@nestjs/sequelize';
+import { SequelizeModuleAsyncOptions } from '@nestjs/sequelize';
 import { Dialect } from 'sequelize/lib/sequelize';
 
-export const sequelizeConfig = (
-  configService: ConfigService,
-): SequelizeModuleOptions => ({
-  database: configService.get<string>('DB_NAME'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT')
-    ? Number(configService.get<number>('DB_PORT'))
-    : 3306,
-  dialect: configService.get<Dialect>('DB_DIALECT') ?? 'mysql',
-  synchronize: true,
-  autoLoadModels: true,
-  models: [Category, Group, Permission, GroupPermission, Account],
-});
+export const sequelizeConfig: SequelizeModuleAsyncOptions = {
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    database: configService.get<string>('DB_NAME'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    host: configService.get<string>('DB_HOST', 'localhost'),
+    port: configService.get<number>('DB_PORT'),
+    dialect: configService.get<Dialect>('DB_DIALECT'),
+    synchronize: true,
+    autoLoadModels: true,
+    models: [Category, Group, Permission, GroupPermission, Account],
+  }),
+};
