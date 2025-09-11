14faf89004d1250e2412554d8b14cc91815b6b39
huypham <huyphuocbinh204@gmail.com>
2025-09-11 14:43:50 +0700

    feat: add publisher to product

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -4,13 +4,15 @@ import { ProductController } from './product.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Product } from '@/models/product.model';
 import { CategoryModule } from '../category/category.module';
+import { PublisherModule } from '../publisher/publisher.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
   imports: [
     SequelizeModule.forFeature([Product]),
-    forwardRef(() => CategoryModule)
+    forwardRef(() => CategoryModule),
+    PublisherModule
   ],
   exports: [ProductService]
 })
