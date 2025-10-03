69a0e812acd275fab8071e1ef674fa1669c61891
LeTanTru <letantru@gmail.com>
2025-10-03 16:38:45 +0700

    feat: update filter coupon

--- src/modules/coupon/dtos/coupon-auto-complete.dto.ts
+++ src/modules/coupon/dtos/coupon-auto-complete.dto.ts
@@ -14,6 +14,9 @@ export class CouponAutoCompleteDto {
   name: string;
 
   @Expose()
+  description: string;
+
+  @Expose()
   type: number;
 
   @Expose()
--- src/modules/coupon/forms/filter-coupon.form.ts
+++ src/modules/coupon/forms/filter-coupon.form.ts
@@ -40,7 +40,7 @@ export class FilterCouponForm extends PaginationForm {
     if (this.kind) where.kind = this.kind;
     if (this.type) where.type = this.type;
     if (this.minOrderAmount)
-      where.minOrderAmount = { [Op.gt]: `%${this.minOrderAmount}%` };
+      where.minOrderAmount = { [Op.lt]: this.minOrderAmount };
     if (this.status) where.status = this.status;
     if (this.id) where.id = this.id;
 
